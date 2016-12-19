6daa69a60dcfd4f42272b0fa777b3660a53b4f2e
BertoliAlessandro <alessandro.bertoli@register.it>
2016-12-19 10:37:42 +0100

    first commit

---
+++ CompitoTpsi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compitotpsi;
+
+/**
+ *
+ * @author studente
+ */
+public class CompitoTpsi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic her
+        for (int i=1; i<11; i++)
+        {
+            System.out.println(i);
+        }
+            
+            
+    }
+    
+}
